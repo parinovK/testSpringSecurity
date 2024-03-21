7dfa6b9082632c073840a040a16b97035550b36f
parinovK <parinovk@yandex.ru>
2024-03-21 20:01:27 +0300

    init

---
+++ src/main/java/ru/parinov/testSpringSecurity/TestSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.parinov.testSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringSecurityApplication.class, args);
+	}
+
+}
